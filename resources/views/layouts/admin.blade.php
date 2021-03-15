48d9181f6e20d47effd011534f11b9756bc2f844
miki saki <ss10863008@gmail.com>
2021-03-15 11:40:48 +0000

    1st commit

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lamg="{{ app()->getLocale() }}">
+    <head>
+        <meta chaeset="utf-8">
+        <meta http-equiv="X-UA-Compatoble" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- CSRF Token -->
+        {{-- 後の章で説明します --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        {{--各ページごとにtitleタグを入れるために@yieldで空けておきます。 --}}
+        <title>@yield('title')</title>
+        
+        <!-- Scripts -->
+        {{--Laravel標準で用意されているJavascriptを読み込みます --}}
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+        
+        <!--Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+        
+        <!-- Style -->
+        {{ Laravel標準で用意されているCSSを読み込みます --}}
+        <link href="{{ seurce_asset('css/app.css') }}" rel="stylesheet">
+        {{-- この章の後半で作成するcssを読み込みます --}}
+        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            {{-- 画面上部に表示するナビゲーションバーです。 --}}
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+                <dev class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria
+                </dev>
+            </nav>
+        </div>
+    </body>
+</html>
